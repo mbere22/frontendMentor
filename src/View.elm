155545d4871a9c40886165522ca9cf904c88065a
mbere22 <igihembwecyambere2022@gmail.com>
2022-05-30 04:51:55 +0000

    E commit

--- src/View.elm
+++ src/View.elm
@@ -1,19 +1,40 @@
 module View exposing (view)
 
 import Element as E
+import Element.Background as EB
+import Element.Font as EF
 
+--Debug
 x = E.explain Debug.todo
-pw = \a -> w (E.px a)
-ph = \a -> w (E.px a)
+
+--Size Height Width
 h = E.height
 w = E.width  
 fh = h E.fill
 fw = w E.fill
 fph = \a->h (E.fillPortion a)
 fpw = \a -> w (E.fillPortion a)
-tresx = 48
 
-view = E.layout [x] body
+--Color
+ca = E.rgb255 252 218 19 
+--cb =
+--cc =
+--cd =
+--ce =
+--cd = 
+--cf =
+--ce =
+rgb = \a -> EB.color a
+
+--Font 
+
+
+
+view = E.layout [x
+    ,EF.color (E.rgb 0 0 1)
+    , EF.size 18
+    , EF.family[EF.external { name = "Overpass", url = "https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap"}
+    , EF.sansSerif]] body
 
 body = E.column [fw, fh] [uno, dos, tres, adro]
 
@@ -22,11 +43,11 @@ dos = E.column [fw, fph 4] [dosa, dosb]
 tres = E.row [fw, fph 2] [tresa, tresb, tresc, tresd, trese]
 adro = E.row [fw, fph 2] [adroa]
 
-unoa = E.el [x, fpw 1] (E.text "Uno") 
+unoa = E.el [x, fpw 1, rgb ca] (E.text "Uno") 
 unob = E.el [x, fpw 9] (E.text "Uno")
 
-dosa= E.el [x, fph 3, fw] (E.text "dos a")
-dosb = E.el [x, fph 7, fw] (E.text "dos b")
+dosa= E.el [x, fph 3, fw] (E.text "How did we do?")
+dosb = E.el [x, fph 7, fw] (E.text "Please let us know how we did with you r support request. Sll feddback is appreciated to help us improve offering!")
 
 tresa = E.el [x, fw] (E.text "tresa")
 tresb = E.el [x, fw] (E.text "tresb")

a4e930bc86587cdd28646c003ef646266e14c3a6
mbere22 <igihembwecyambere2022@gmail.com>
2022-06-03 01:30:30 +0000

    Faq design

--- src/View.elm
+++ src/View.elm
@@ -3,9 +3,11 @@ module View exposing (view)
 import Element as E
 import Element.Background as Eb
 import Element.Border as Ebr
-import Svg as S 
-import Svg.Attributes as Sa 
+import Element.Font as Ef
 import Icon as I
+import Html as H
+import Html.Attributes as Ha
+import Element.Input as Ei
 
 --VALUES
 va = fb 231 231 233
@@ -18,7 +20,7 @@ vg = fb 120 120 135
 vh = "12px"
 vi = "375px"
 vj = "1440px"
-vk = "https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap"
+vk = "https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
 vl = {angle = pi, steps = [vd, ve]} --view background gradient color
 vm = fb 255 255 255
 vn = E.centerX
@@ -28,6 +30,13 @@ vq = E.clip
 vr = E.fill
 vs = E.html <| I.b
 vt = E.html <| I.c 
+vu = "How many team members can I invite?"
+vv = "What is the maximum file upload size?"
+vw = "How do I reset my password?"
+vx = "Can I cancel my subscription?"
+vy = "Do you provide additional support?"
+vz = "FAQ"
+vaa = E.html <| H.hr [] []
 
 --FUNCTIONS
 fa = \a -> Eb.color a  --set color to background
@@ -42,12 +51,16 @@ fh = \a -> Ebr.rounded a
 fi = \a -> E.moveLeft a
 fj = \a -> E.moveUp a
 fk = \a -> E.above a
+fl = \a -> E.text a
+fm = \a -> E.spacing a 
 
 view = E.layout [fc vl] body
 body = E.column [fa vm, vn, vo, fe <| fg 450, ff <| fg 950, fh 24] [uno]
 uno = E.row [ff vr, fe vr] [unoa, unob] 
-unoa = E.column [ff vr, fe vr, x, E.inFront unoab, E.inFront unoac][unoaa] 
-unob = E.column [ff vr, fe vr][E.none]
+unoa = E.column [ff vr, fe vr, E.inFront unoab, E.inFront unoac][unoaa] 
+unob = E.column [ff vr, fe vr][unoba, unobb]
+unoba = E.column [ff vr, fe vr, fe <| fd 1][E.el [Ef.size 30, Ef.bold, vo, Ef.family [ Ef.external { name = "Kumbh Sans", url = vk }, Ef.sansSerif]] <| fl vz] 
+unobb = E.column [ff vr, fe vr, fm 20, fe <| fd 3] [Ei.button [Ef.size 12, Ef.family [ Ef.external { name = "Kumbh Sans", url = vk }, Ef.sansSerif]]{onPress= Nothing, label=E.text vu}, vaa, Ei.button [Ef.size 12, Ef.family [ Ef.external { name = "Kumbh Sans", url = vk }, Ef.sansSerif]]{onPress= Nothing, label=E.text vv}, vaa, fl vw, vaa, fl vx, vaa, fl vy, vaa]
 unoaa = E.el [ff vr, fe vr] vp
 unoab = E.el [ff vr, fe vr] vs
 unoac = E.el [ff vr, fe vr] vt 
